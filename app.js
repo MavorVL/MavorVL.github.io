1005b2171afb2690094426478f384f772c5a22f8
MavorVL <60918380+MavorVL@users.noreply.github.com>
2024-01-29 00:45:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ let p = document.createElement("p");
 //p.innerText = `${tg.initDataUnsafe.user.first_name}
 //${tg.initDataUnsafe.user.last_name}`;
 
-usercard.appendChild(p);
+//usercard.appendChild(p);
 
 
 
